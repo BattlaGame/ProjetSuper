e84964e43a3545edd6fffff28b3f1e9f4498aee1
Corentin Maury <you@example.com>
2023-03-03 09:28:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projetsuper/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.projetsuper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
