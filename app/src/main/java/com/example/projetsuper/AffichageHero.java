a26af4d8051ab5e63c804eb13c26633dbc4b6ecf
thomas <thomasmichaud41@gmail.com>
2023-03-07 09:29:24 +0100

    modifiy affichage hero

--- app/src/main/java/com/example/projetsuper/AffichageHero.java
+++ app/src/main/java/com/example/projetsuper/AffichageHero.java
@@ -1,8 +1,32 @@
 package com.example.projetsuper;
 
+import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class AffichageHero {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AffichageHero extends AppCompatActivity {
 
     ProgressBar intelligence;
+    ProgressBar force;
+    ProgressBar vitesse;
+    ProgressBar durabilite;
+    ProgressBar pouvoir;
+    ProgressBar combat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.affichage_heros);
+        intelligence = (ProgressBar) findViewById(R.id.bar_intelligence);
+        force = (ProgressBar) findViewById(R.id.bar_force);
+        vitesse = (ProgressBar) findViewById(R.id.bar_vitesse);
+        durabilite = (ProgressBar) findViewById(R.id.bar_durabilite);
+        pouvoir = (ProgressBar) findViewById(R.id.bar_pouvoir);
+        combat = (ProgressBar) findViewById(R.id.bar_combat);
+
+
+    }
 }
--- app/src/main/java/com/example/projetsuper/MainActivity.java
+++ app/src/main/java/com/example/projetsuper/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.projetsuper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.affichage_heros);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void hero(View v){
+        Intent ia = new Intent (MainActivity.this, AffichageHero.class);
     }
 }
