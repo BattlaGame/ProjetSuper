1d1fbfcb7f691f9ed454d4f58053b1d3d73a6d0b
maury <maury.corentin18@gmail.com>
2023-03-07 10:46:52 +0100

    Classe Affichage Hero

--- app/src/main/java/com/example/projetsuper/MainActivity.java
+++ app/src/main/java/com/example/projetsuper/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.button_hero);
     }
 
     public void hero(View v){
